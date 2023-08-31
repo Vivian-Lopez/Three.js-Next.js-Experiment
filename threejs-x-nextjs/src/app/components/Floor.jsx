0f07ced9b64eccee802ac345e9caa85888f1afa6
Vivian Lopez <vivianlopez777@gmail.com>
2023-09-01 00:29:38 +0100

    Add Draggable Capabilities to box

--- threejs-x-nextjs/src/app/components/Floor.jsx
+++ threejs-x-nextjs/src/app/components/Floor.jsx
@@ -1,8 +1,4 @@
 import React from "react";
-// import { extend } from '@react-three/drei'
-// import { meshPhysicalMaterial } from 'three'; // Import 'meshPhysicalMaterial' from 'three'
-// import { Box } from '@react-three/fiber'; // Import 'Box' from '@react-three/fiber'
-// import {boxBufferGeometry} from '@react-three/drei'
 
 function Floor(props) {
     return (
--- threejs-x-nextjs/src/app/page.js
+++ threejs-x-nextjs/src/app/page.js
@@ -1,15 +1,46 @@
 "use client"
 
-import React from 'react'
-import css from "./globals.css"
-import { Canvas } from '@react-three/fiber';
-import Floor from "./components/Floor";
-import LightBulb from './components/LightBulb';
-import Box from './components/Box'
-import { OrbitControls } from './components/OrbitControls';
-import Controls from './components/OrbitControls';
+// import React from 'react'
+// import css from "./globals.css"
+// import { Canvas } from '@react-three/fiber';
+// import Floor from "./components/Floor";
+// import LightBulb from './components/LightBulb';
+// import Box from './components/Box'
+// import { OrbitControls } from './components/OrbitControls';
+// import Controls from './components/OrbitControls';
+// import Draggable from './components/Draggable';
 
-export default function page() {
+// export default function page() {
+//   return (
+//     <div className={css.scene}>
+//       <Canvas
+//         shadows
+//         className={css.canvas}
+//         camera={{
+//           position: [-6, 7, 7],
+//         }}
+//       >
+//         {/* The ambientLight component creates the scene white */}
+//         <ambientLight color={"white"} intensity={0.3} />
+//         <LightBulb position={[0, 3, 0]} />
+//         <Draggable>
+//           <Box rotateX={3} rotateY={0.2} />
+//         </Draggable>
+//         <Controls />
+//         <Floor position={[0, -1, 0]} />
+//       </Canvas>
+//     </div>
+//   );
+// }
+import css from "./globals.css";
+import { Canvas } from "@react-three/fiber";
+import Box from "./components/Box";
+import OrbitControls from "./components/OrbitControls";
+import Light from "./components/LightBulb";
+import Floor from "./components/Floor";
+import Draggable from "./components/Draggable";
+
+export default function Home() {
   return (
     <div className={css.scene}>
       <Canvas
@@ -19,12 +50,13 @@ export default function page() {
           position: [-6, 7, 7],
         }}
       >
-        {/* The ambientLight component creates the scene white */}
-        <ambientLight color={"white"} intensity={0.3} />
-        <LightBulb position={[0, 3, 0]} />
-          <Box rotateX={3} rotateY={0.2} />
-        <Controls />
-        <Floor position={[0, -1, 0]} />
+        <ambientLight color={"white"} intensity={0.2} />
+        {/* <Light position={[0, 3, 0]} /> */}
+        {/* <Draggable> */}
+            <Box rotateX={3} rotateY={0.2} />
+        {/* </Draggable> */}
+        <OrbitControls />
+        {/* <Floor position={[0, -1, 0]} /> */}
       </Canvas>
     </div>
   );
